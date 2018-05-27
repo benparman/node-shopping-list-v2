3b6eb23fe52e3d419a1a6d1c905e251f6a1cde63
Ben Parman <benparman@gmail.com>
2018-05-27 13:08:49 -0600

    Added POST endpoint for Recipes

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ Recipes.create(
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
-
 app.post('/shopping-list', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
   const requiredFields = ['name', 'budget'];
@@ -42,15 +41,26 @@ app.post('/shopping-list', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-
   const item = ShoppingList.create(req.body.name, req.body.budget);
   res.status(201).json(item);
 });
-
-
+//=========================================
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
+app.post('/recipes', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = Recipes.create(req.body.name, req.body.ingredients)
+  res.status(201).json(item);
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
